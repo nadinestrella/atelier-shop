6132e1a5f54177e05352a027262869a78e4c88f3
nadinestrella <nadinestrella@gmail.com>
2026-03-11 15:47:26 +0100

    checkoutPage- improved layout and errors

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -35,195 +35,193 @@ export default function CheckoutPage() {
       <h2 className="text-3xl tracking-wider mb-12">CHECKOUT</h2>
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-        <fieldset className="grid grid-cols-2 gap-4">
+        <fieldset className="flex flex-col gap-6">
           <legend className="text-lg tracking-wider mb-6">
             CONTACT INFORMATION
           </legend>
 
-          <input
-            placeholder="First Name"
-            type="text"
-            {...register('firstName', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid First Name',
-              },
-            })}
-            className="w-full border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              placeholder="First Name"
+              type="text"
+              {...register('firstName', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid First Name',
+                },
+              })}
+              className="w-full border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="Last Name"
-            type="text"
-            {...register('lastName', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid Last Name',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="Last Name"
+              type="text"
+              {...register('lastName', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid Last Name',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="Email"
-            type="email"
-            {...register('email', {
-              required: true,
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: 'Invalid email',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="Email"
+              type="email"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: 'Invalid email',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
+          </div>
 
           {/* ERRORS */}
-          {(errors.firstName || errors.lastName || errors.email) && (
-            <span className="text-red-500 text-sm">
-              {errors.firstName?.message ||
-                errors.lastName?.message ||
-                errors.email?.message}
-            </span>
-          )}
+          <p className="text-red-500 text-xs mt-1 h-4">
+            {errors.firstName?.message ||
+              errors.lastName?.message ||
+              errors.email?.message}
+          </p>
         </fieldset>
 
-        <fieldset className="grid grid-cols-2 gap-4">
+        <fieldset className="flex flex-col gap-6">
           <legend className="text-lg tracking-wider mb-6">
             SHIPPING ADDRESS
           </legend>
-          <input
-            placeholder="Street Address"
-            type="text"
-            {...register('streetAddress', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid Street Address',
-              },
-            })}
-            className="w-full border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              placeholder="Street Address"
+              type="text"
+              {...register('streetAddress', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid Street Address',
+                },
+              })}
+              className="w-full border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="City"
-            type="text"
-            {...register('city', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid City',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="City"
+              type="text"
+              {...register('city', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid City',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="State"
-            type="text"
-            {...register('state', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid State',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="State"
+              type="text"
+              {...register('state', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid State',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="Zip Code"
-            type="text"
-            {...register('zipCode', {
-              required: true,
-              pattern: {
-                value: /^\d{5}$/,
-                message: 'Invalid Zip Code',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="Zip Code"
+              type="text"
+              {...register('zipCode', {
+                required: true,
+                pattern: {
+                  value: /^\d{5}$/,
+                  message: 'Invalid Zip Code',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="Country"
-            type="text"
-            {...register('country', {
-              required: true,
-              pattern: {
-                value: /^[a-zA-Z\s]+$/,
-                message: 'Invalid Country',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="Country"
+              type="text"
+              {...register('country', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Invalid Country',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
+          </div>
           {/* ERRORS */}
-          {(errors.streetAddress ||
-            errors.city ||
-            errors.state ||
-            errors.zipCode ||
-            errors.country) && (
-            <span className="text-red-500 text-sm">
-              {errors.streetAddress?.message ||
-                errors.city?.message ||
-                errors.state?.message ||
-                errors.zipCode?.message ||
-                errors.country?.message}
-            </span>
-          )}
+
+          <p className="text-red-500 text-xs mt-1 h-4">
+            {errors.streetAddress?.message ||
+              errors.city?.message ||
+              errors.state?.message ||
+              errors.zipCode?.message ||
+              errors.country?.message}
+          </p>
         </fieldset>
 
-        <fieldset className="grid grid-cols-2 gap-4">
+        <fieldset className="flex flex-col gap-6">
           <legend className="text-lg tracking-wider mb-6">
             PAYMENT DETAILS
           </legend>
-          <input
-            placeholder="Card Number"
-            type="number"
-            {...register('cardNumber', {
-              required: true,
-              pattern: {
-                value: /^\d{16}$/,
-                message: 'Invalid Card Number',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              placeholder="Card Number"
+              type="number"
+              {...register('cardNumber', {
+                required: true,
+                pattern: {
+                  value: /^\d{16}$/,
+                  message: 'Invalid Card Number',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="MM/YY"
-            type="text"
-            {...register('expiryDate', {
-              required: true,
-              pattern: {
-                value: /^(0[1-9]|1[0-2])\/\d{2}$/,
-                message: 'Invalid Expiry Date',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="MM/YY"
+              type="text"
+              {...register('expiryDate', {
+                required: true,
+                pattern: {
+                  value: /^(0[1-9]|1[0-2])\/\d{2}$/,
+                  message: 'Invalid Expiry Date',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
 
-          <input
-            placeholder="CVV"
-            type="number"
-            {...register('cvv', {
-              required: true,
-              pattern: {
-                value: /^\d{3,4}$/,
-                message: 'Invalid CVV',
-              },
-            })}
-            className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-          />
+            <input
+              placeholder="CVV"
+              type="number"
+              {...register('cvv', {
+                required: true,
+                pattern: {
+                  value: /^\d{3,4}$/,
+                  message: 'Invalid CVV',
+                },
+              })}
+              className="border border-black px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black"
+            />
+          </div>
 
           {/* ERRORS */}
-          {(errors.cardNumber || errors.expiryDate || errors.cvv) && (
-            <span className="text-red-500 text-sm">
-              {errors.cardNumber?.message ||
-                errors.expiryDate?.message ||
-                errors.cvv?.message}
-            </span>
-          )}
+
+          <p className="text-red-500 text-xs mt-1 h-4">
+            {errors.cardNumber?.message ||
+              errors.expiryDate?.message ||
+              errors.cvv?.message}
+          </p>
         </fieldset>
 
         <button
