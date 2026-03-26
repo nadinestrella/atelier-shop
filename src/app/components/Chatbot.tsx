3e6aa74c182719708a4c75e6ab2d340511205cdf
nadinestrella <nadinestrella@gmail.com>
2026-03-26 12:19:13 +0100

    chatbot- logic and style

--- src/app/components/Chatbot.tsx
+++ src/app/components/Chatbot.tsx
@@ -1,36 +1,117 @@
+'use client';
+
+import { MessageCircle, X } from 'lucide-react';
+import { useState } from 'react';
+
+type Message = {
+  text: string;
+  from: 'user' | 'bot';
+};
+
 export default function Chatbot() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState<string>('');
+  const [isChatbotOpen, setIsChatbotOpen] = useState<boolean>(false);
+
+  const getBotResponse = (message: string) => {
+    const msg = message.toLowerCase();
+
+    if (msg.includes('shipment')) {
+      return 'Shipping takes between 24 and 48 hours.';
+    }
+
+    if (msg.includes('return')) {
+      return 'You can return your order within 30 days.';
+    }
+    if (msg.includes('size')) {
+      return 'We recommend you check our size guide.';
+    }
+    return 'I am not sure about that, but I can help you with shipping, returns, or sizes.';
+  };
+
+  const handleSend = () => {
+    if (!input.trim()) return;
+    const userMessage: Message = { text: input, from: 'user' };
+    const botMessage: Message = {
+      text: getBotResponse(input),
+      from: 'bot',
+    };
+    setMessages((prev) => [...prev, userMessage, botMessage]);
+    setInput('');
+  };
+
   return (
-    <div className="fixed bottom-4 right-4 w-80 border border-gray-400">
-      <div className="bg-black text-white flex flex-col p-2">
-        <p className="font-semibold">Atelier bot</p>
-        <span className="text-sm">Available</span>
-      </div>
+    <div className="fixed bottom-11 right-4 w-80  shadow-2xl z-20">
+      <button
+        onClick={() => setIsChatbotOpen((prev) => !prev)}
+        aria-label="Close chatbot"
+        className="bg-black text-white p-2 rounded-full shadow-lg mb-2 fixed right-3 bottom-0.5 "
+      >
+        {isChatbotOpen ? (
+          <X className="size-6" />
+        ) : (
+          <MessageCircle className="size-6" />
+        )}
+      </button>
 
-      {/* Messages */}
+      {isChatbotOpen && (
+        <div>
+          <div className="bg-black text-white flex flex-col p-2">
+            <p className="font-semibold">Atelier bot</p>
+            <span className="text-sm">Available</span>
+          </div>
 
-      <div className="bg-white p-2.5 flex flex-col gap-2">
-        <div className="border border-gray-200 bg-gray-100 p-1 justify-start">
-          <p className="text-sm ">Hi! How can I help you today?</p>
+          {/* Messages */}
+
+          <div className="bg-white p-2.5 flex flex-col gap-2">
+            <div className="flex justify-end">
+              <p className="px-3 py-2 rounded-lg max-w-[70%] bg-gray-400 text-white text-sm">
+                Hi! How can we help you?
+              </p>
+            </div>
+            {messages.map((msg, i) => (
+              <div
+                key={i}
+                className={`mb-2 flex ${
+                  msg.from === 'user' ? 'justify-end' : 'justify-start'
+                }`}
+              >
+                <div
+                  className={`px-3 py-2 rounded-lg max-w-[70%] text-sm ${
+                    msg.from === 'user'
+                      ? 'bg-gray-400 text-white'
+                      : 'bg-gray-200 text-black'
+                  }`}
+                >
+                  {msg.text}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Input */}
+          <form
+            className="p-2 border-t flex gap-2 bg-white"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              className="flex-1 text-sm border border-gray-200 w-6xl p-0.5"
+              placeholder="Write..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="w-full bg-black text-white p-2 tracking-wider text-sm hover:bg-gray-800 transition-colors disabled:opacity-50 disabled:bg-gray-400"
+              onClick={handleSend}
+            >
+              Send
+            </button>
+          </form>
         </div>
-
-        <div className="border border-gray-200 bg-gray-100 p-1 justify-end">
-          <p className="text-sm ">Returns</p>
-        </div>
-      </div>
-
-      {/* Input */}
-      <div className="p-2 border-t flex gap-2 bg-white">
-        <input
-          className="text-sm border border-gray-200 w-6xl p-0.5"
-          placeholder="Write..."
-        />
-        <button
-          type="submit"
-          className="w-full bg-black text-white p-2 tracking-wider text-sm hover:bg-gray-800 transition-colors disabled:opacity-50 disabled:bg-gray-400"
-        >
-          Send
-        </button>
-      </div>
+      )}
     </div>
   );
 }
